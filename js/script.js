f7c3ef9e0bf6439b8c3dd850ed4bc80a05a7d154
sFrans21 <118550565+sFrans21@users.noreply.github.com>
2024-11-14 14:59:17 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -86,38 +86,38 @@ window.onscroll = () => {
 //     });
 // };
 
-const incrementLikes = (userId, moduleId) => {
-  firestore
-    .collection("users")
-    .doc(userId)
-    .collection("modules")
-    .doc(moduleId)
-    .update({
-      likes: firebase.firestore.FieldValue.increment(1), // Menambah 1 like
-    })
-    .then(() => {
-      console.log("Like berhasil ditambahkan!");
-    })
-    .catch((error) => {
-      console.error("Error menambah like: ", error);
-    });
-};
+// const incrementLikes = (userId, moduleId) => {
+//   firestore
+//     .collection("users")
+//     .doc(userId)
+//     .collection("modules")
+//     .doc(moduleId)
+//     .update({
+//       likes: firebase.firestore.FieldValue.increment(1), // Menambah 1 like
+//     })
+//     .then(() => {
+//       console.log("Like berhasil ditambahkan!");
+//     })
+//     .catch((error) => {
+//       console.error("Error menambah like: ", error);
+//     });
+// };
 
-const decrementLikes = (userId, moduleId) => {
-  firestore
-    .collection("users")
-    .doc(userId)
-    .collection("modules")
-    .doc(moduleId)
-    .update({
-      likes: firebase.firestore.FieldValue.increment(-1), // Mengurangi 1 like
-    })
-    .then(() => {
-      console.log("Like berhasil dikurangi!");
-    })
-    .catch((error) => {
-      console.error("Error mengurangi like: ", error);
-    });
-};
+// const decrementLikes = (userId, moduleId) => {
+//   firestore
+//     .collection("users")
+//     .doc(userId)
+//     .collection("modules")
+//     .doc(moduleId)
+//     .update({
+//       likes: firebase.firestore.FieldValue.increment(-1), // Mengurangi 1 like
+//     })
+//     .then(() => {
+//       console.log("Like berhasil dikurangi!");
+//     })
+//     .catch((error) => {
+//       console.error("Error mengurangi like: ", error);
+//     });
+// };
 
-document.querySelector("#like").onclick = decrementLikes(userId, moduleId);
+// document.querySelector("#like").onclick = decrementLikes(userId, moduleId);
