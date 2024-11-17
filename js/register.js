467e38c72afff32c83fb0019f6aa71c38b9d7e18
sFrans21 <118550565+sFrans21@users.noreply.github.com>
2024-11-17 20:29:37 +0700

    Update register.js

--- js/register.js
+++ js/register.js
@@ -51,7 +51,7 @@ submit.addEventListener("click", function (event) {
         nim: nim,
         faculty: faculty,
         password: password,
-        profilePic: "",
+        profilePicUrl: null,
       };
       alert("Membuat akun...");
       const docRef = doc(db, "user", user.uid);
